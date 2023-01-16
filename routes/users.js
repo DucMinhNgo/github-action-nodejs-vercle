e5922358c45ff9815ab1285dfaa2e8eb2e25f1ba
Ngô Minh Đức <ducminhngo1998@gmail.com>
2023-01-16 17:16:03 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource users');
-  res.json({ name: 'Dustin pro nodemon jenkins tada.' });
+  res.json({ name: 'Dustin pro nodemon jenkins tada trigger not push.' });
 });
 
 module.exports = router;
