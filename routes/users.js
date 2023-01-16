69db2cf80cbef15019181636cd151b5e5386a479
Ngô Minh Đức <ducminhngo1998@gmail.com>
2023-01-16 15:58:17 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource users');
-  res.json({ name: 'Dustin pro' });
+  res.json({ name: 'Dustin pro 1' });
 });
 
 module.exports = router;
